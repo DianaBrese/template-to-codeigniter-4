d547989e5e29d3d324c27960c7da726bc062b525
DianaBrese <dianabrese15@gmail.com>
2024-09-02 17:43:12 -0300

    listagem de posts da categoria escolhida

--- app/Controllers/Category.php
+++ app/Controllers/Category.php
@@ -3,12 +3,33 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\Category as ModelsCAtegory;
+use App\Models\Post;
 
 class Category extends BaseController
 {
-    public function index(string $category)
+    public function index(string $slug)
     {
-        var_dump($category);
+        helper('text');
+        
+        $category = new ModelsCategory();
+        $category = $category->select('categories.id')->where('slug', $slug)->first();
+       
+        $post = new Post();
+        $post->select(
+            'posts.id, posts.image, ,posts.title, posts.created_at, posts.slug, posts.description, categories.name as categoryName, users.firstName as userFirstName, users.lastName as userLastName'
+        )->join(
+            'users',
+            'users.id = posts.user_id '
+        )->join(
+            'categories',
+            'categories.id = posts.category_id'
+        )->where(
+            'posts.category_id',
+            $category->id
+        );
+
+        
+        return view('category', ['posts'=>$post->paginate(5), 'pager' =>$post->pager]);
     }
 }
--- system/Pager/Views/default_full.php
+++ system/Pager/Views/default_full.php
@@ -24,24 +24,13 @@ $pager->setSurroundCount(2);
 		<?php endif ?>
 
 		<?php foreach ($pager->links() as $link) : ?>
-			<li <?= $link['active'] ? 'class="active"' : '' ?>>
-				<a href="<?= $link['uri'] ?>">
+			<li >
+				<a href="<?= $link['uri'] ?>" <?= $link['active'] ? 'class="active"' : '' ?>>
 					<?= $link['title'] ?>
 				</a>
 			</li>
 		<?php endforeach ?>
 
-		<?php if ($pager->hasNext()) : ?>
-			<li>
-				<a href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
-					<span aria-hidden="true"><?= lang('Pager.next') ?></span>
-				</a>
-			</li>
-			<li>
-				<a href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>">
-					<span aria-hidden="true"><?= lang('Pager.last') ?></span>
-				</a>
-			</li>
-		<?php endif ?>
+		
 	</ul>
 </nav>
